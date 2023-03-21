d8a098d3d1e49e8a8da7dd4a0839717762f22e1c
Kshitij Tyagi <kshitijtyagi03@gmail.com>
2023-03-21 02:40:50 -0700

    code with some frontend edits and axios call working but no database aspect

--- flask_react/src/App.js
+++ flask_react/src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react'
 import { useState } from 'react'
 import axios from "axios";
 import logo from './logo.svg';
@@ -15,7 +16,7 @@ function App() {
     })
     .then((response) => {
       const res =response.data
-      alert(res)
+      //alert(res)
       setProfileData(({
        profile_name: res.name,
         about_me: res.about}))
@@ -25,27 +26,31 @@ function App() {
         console.log(error.response.headers)
         }
     })}
-    //end of new line 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+    //end of new line  <img src={logo} className="App-logo" alt="logo" /> 
+    //Edit <code>src/App.js</code> and save to reload.
+    /*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */
+  return (
+    <div className="App">
+      <header className="App-header">
+         
+        <h2>Open Avenues Internship - Data Scrubber Application</h2>
+        <p>
+          Created by Kshitij Tyagi under guidance of Rishabh Gupta.
+          
+        </p>
+        
 
         {/* new line start*/}
         <p>To get your profile details: </p><button onClick={getData}>Click me</button>
         {profileData && <div>
+              <small>User ID    First Name    Last Name    Data    Age    Email </small>
               <p>Profile name: {profileData.profile_name}</p>
               <p>About me: {profileData.about_me}</p>
             </div>
